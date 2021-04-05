93d4138297dbbc55b896826d41f8a92ca2d99586
KoitoLov <victor.nunez.roa5@gmail.com>
2021-04-05 13:03:37 -0400

    A lot of changes

---
+++ PythonFiles/personalLearning/Metodos.py
@@ -0,0 +1,11 @@
+
+
+while jugando:
+
+
+print("Eliga un jugador")
+print("Pulse 1 para jugador A")
+print("Pulse 2 para jugador B")
+print("Pulse 3 para jugador C")
+print("/n")
+int(input("Ingrese su opcion: "))
