9aeb62946e4445c52af3ed10a28f0c43aa7cfaa3
KoitoLov <victor.nunez.roa5@gmail.com>
2021-04-09 16:36:48 -0400

    Guia4 Hasta el ejercicio 4
    El ejercicio 5 es WIP

---
+++ UaiProgramacion/Guia4Listas.py
@@ -0,0 +1,83 @@
+import random
+
+
+
+
+#Primer ejercicio.
+
+# lista = []
+
+# for i in range(5):
+#     num = int(input("Ingrese un num: "))
+#     lista.append(num)
+
+# print(lista)
+
+
+
+#Segundo ejercicio.
+
+# rango = int(input("Ingrese un num: "))
+
+# lista = []
+# suma = 0
+
+# for i in range(rango):
+#     num = int(input("Numero a la lista: "))
+#     suma += num
+#     lista.append(num)
+    
+# promedio = suma/rango
+
+# print(lista)
+# print(promedio)
+
+
+
+#Tercer ejercicio.
+
+# lista = []
+
+# for i in range(20):
+#     num_random = random.randint(0, 20)
+#     lista.append(num_random)
+
+# print(lista)
+
+
+
+#Cuarto ejercicio.
+
+# lista = []
+# lista_nueva = lista
+
+# for i in range(30):
+#     num_random = random.randint(0, 30)
+#     lista.append(num_random)
+
+# lista_nueva.sort()
+
+# print("La lista es:", lista)
+# print("El segundo menor es:", (lista_nueva[1]))
+
+
+
+#Quinto ejercicio.
+
+lista = []
+posicion_lista = []
+
+
+for i in range(40):
+    num_random = random.randint(0, 40)
+    lista.append(num_random)
+
+num = int(input("Ingrese un num: "))
+
+if num in lista:
+    
+
+print(posicion_lista)
+                
+
+
